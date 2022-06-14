66b78b2e9b5cd3d8150925ad0e6f7aecca0f9461
SanFili <filimonova.alexandra@yandex.ru>
2022-06-14 18:17:48 +0300

    chessboard

--- src/tasks/Chessboard/Chessboard.jsx
+++ src/tasks/Chessboard/Chessboard.jsx
@@ -1,22 +1,13 @@
 import React from "react";
 import "./Chessboard.scss";
-import cn from "classnames";
 
 const Chessboard = () => {
-  const chessSquares = [
-    0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0,
-    1, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0,
-  ];
 
   return (
     <>
       <h2>Шахматная доска</h2>
       <div className="chessboard">
-        <div className="chessboard__board">
-          {chessSquares.map((el, i) => (
-            <div className={cn("chessboard__square", el && "chessboard__square_black")} key={i} />
-          ))}
-        </div>
+        <div className="chessboard__board"/>
       </div>
     </>
   );
